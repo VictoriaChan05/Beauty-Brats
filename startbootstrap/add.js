14ef0ab87345e00f970cba9d60d2dc30f7a7938a
Sarah Bui <sarahb2277@gmail.com>
2019-12-05 17:53:02 -0600

    add and delete

--- startbootstrap/add.js
+++ startbootstrap/add.js
@@ -1,5 +1,32 @@
-//make constants
+let myItems = [];
 
-//add function
+const addItem = (ev)=>{
+    ev.preventDefault();
+    let myItem = document.getElementById('name').value;
+    myItems.push(myItem);
+    document.querySelector('form').reset();
+    localStorage.setItem('myItemsList', JSON.stringify(myItems));
+}
 
-//delete function
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click',addItem);
+});
+
+
+
+const removeItem = (ev)=>{
+    ev.preventDefault();
+    function removeItem(arr, value){
+        return arr.filter(function(ele){
+            return ele != value;
+        });
+    }
+    let remItem = document.getElementById('name').value;
+    myItems = removeItem(myItems, remItem);
+    document.querySelector('form').reset();
+    localStorage.setItem('myItemsList', JSON.stringify(myItems));
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('deleteBtn').addEventListener('click',removeItem);
+});
